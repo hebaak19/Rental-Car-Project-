66a82ac486bb91bdfc246be74c8a5a99b24615a3
hebaak19 <hebaalkathiri9@gmail.com>
2025-12-04 13:03:33 +0300

    feat: seperate error messages from the main

---
+++ UI/ErrorMessages.java
@@ -0,0 +1,13 @@
+package UI;
+
+public class ErrorMessages {
+    public static final String INVALID_NAME = "Invalid name format. Name should contain only letters and spaces and be at least 2 characters long.";
+    public static final String INVALID_ID = "Invalid ID format. ID should contain only digits and be at least 4 characters long.";
+    public static final String INVALID_EMAIL = "Invalid email format. Please enter a valid work email.";
+    public static final String INVALID_PHONE_NUMBER = "Invalid phone number format. Phone number should contain exactly 10 digits.";
+    public static final String INVALID_LICENSE_PLATE = "Invalid license plate format. License should contain exactly 10 digits.";
+    public static final String INVALID_CAR_ID = "Invalid Car ID format. ID should contain only digits and be at least 4 characters long.";
+    public static final String CAR_ID_EXISTS = "Car ID already exists. Cannot add duplicate ID.";
+    public static final String CAR_NOT_AVAILABLE = "Car not available for rent.";
+    public static final String NO_EMPLOYEES = "No employee found with the provided credentials. Access denied.";
+}
